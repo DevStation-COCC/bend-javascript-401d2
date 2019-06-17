e356e9abf6a1f5bcc1c0b80023b2c24b1c1d0737
DukeOfEtiquette <duquetteadam@gmail.com>
2019-06-17 07:56:22 -0700

    class-15 demo

---
+++ curriculum/class-15/demo/binary-tree/binarySearchTree.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const Node = require('./node/node');
+
+class BinarySearchTree {
+  constructor(node){
+    this.root = node;
+  }
+
+
+}
+
+exports.BinarySearchTree = BinarySearchTree;
